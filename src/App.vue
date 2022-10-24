2dbd1bfd63c1a08a5c9f434250b3c4a7c7b80268
kewennnnn <65531086+kewennnnn@users.noreply.github.com>
2022-10-24 23:12:41 +0800

    Add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,62 @@
 <template>
+	<div class="app">
+		<!-- Sidebar -->
+		<TabSidebar />
+
+		<!-- Content -->
+		<router-view />
+	</div>
+</template>
+
+<script setup>
+import TabSidebar from './components/TabSidebar.vue'
+</script>
+
+<style lang="scss">
+:root {
+	--primary: #4ade80;
+	--primary-alt: #22c55e;
+	--grey: #64748b;
+	--dark: #1e293b;
+	--dark-alt: #334155;
+	--light: #f1f5f9;
+	--sidebar-width: 300px;
+}
+
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Fira sans', sans-serif;
+}
+
+body {
+	background: var(--light);
+}
+
+button {
+	cursor: pointer;
+	appearance: none;
+	border: none;
+	outline: none;
+	background: none;
+}
+
+.app {
+	display: flex;
+
+	main {
+		flex: 1 1 0;
+		padding: 2rem;
+
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
+</style>
+
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -23,4 +81,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
